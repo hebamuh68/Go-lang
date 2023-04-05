209d44d6981e82d11354be978776db3021b67960
hebamuh68 <hebatuallah.20349064@compit.aun.edu.eg>
2023-04-05 21:35:39 +0200

    gfs

---
+++ GFS/Server/go.mod
@@ -0,0 +1,3 @@
+module gfs.com/server
+
+go 1.20
---
+++ GFS/Server/packages/read_data.go
@@ -0,0 +1,17 @@
+package GFS
+
+import (
+	"io/ioutil"
+
+)
+
+func Read_Data(fileName string) string{
+
+	// Open the file for reading
+	file, _ := ioutil.ReadFile(fileName)
+
+	// Convert byte slice to string
+	str := string(file)
+
+	return str
+}
